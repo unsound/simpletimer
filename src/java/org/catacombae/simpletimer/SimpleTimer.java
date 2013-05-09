3fa5290b2ebcff126c29e982f975e0a04dfb5be7
Erik Larsson <catacombae@gmail.com>
2013-05-09 07:49:45 +0200

    Add 'New window' functionality to SimpleTimer.
    This enables SimpleTimer to launch multiple windows within the same
    instance of the application.
    
    This is important in Mac OS X because an application can normally only
    be launched in one instance at a time per user (you can force a launch
    of a second instance through the command line, but that's not supported
    and can cause other problems).

--- src/java/org/catacombae/simpletimer/SimpleTimer.java
+++ src/java/org/catacombae/simpletimer/SimpleTimer.java
@@ -3,11 +3,14 @@ package org.catacombae.simpletimer;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 public class SimpleTimer extends JFrame {
@@ -29,13 +32,26 @@ public class SimpleTimer extends JFrame {
 
 	setupMenus();
 
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	getContentPane().add(mainPanel);
 	pack();
 	setLocationRelativeTo(null);
     }
 
     private void setupMenus() {
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem newWindowMenuItem = new JMenuItem("New window");
+        newWindowMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                (new SimpleTimer()).setVisible(true);
+            }
+        });
+        newWindowMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+                Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+
+        fileMenu.add(newWindowMenuItem);
+
 	JRadioButtonMenuItem traditionalOption = new JRadioButtonMenuItem("Traditional");
 	traditionalOption.setSelected(true);
 	traditionalOption.addActionListener(new ActionListener() {
@@ -59,6 +75,7 @@ public class SimpleTimer extends JFrame {
 	optionsMenu.add(traditionalOption);
 	optionsMenu.add(fixedTimeOption);
 	JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu);
         //menuBar.add(optionsMenu);
 	setJMenuBar(menuBar);
     }
